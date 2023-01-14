49eef758228ad04796363d229c8b2cc67aeae058
AlviGeo <alvigeovan29@gmail.com>
2023-01-14 15:34:33 +0700

    learn unit sub test

--- belajar-golang-unit-test/helper/hello_world_test.go
+++ belajar-golang-unit-test/helper/hello_world_test.go
@@ -9,6 +9,58 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name string
+		request string
+		expected string
+	} {
+		{
+			name: "Alvi",
+			request: "Alvi",
+			expected: "Hello Alvi",
+		},
+		{
+			name: "Geovanny",
+			request: "Geovanny",
+			expected: "Hello Geovanny",
+		},
+		{
+			name: "Zoey",
+			request: "Zoey",
+			expected: "Hello Zoey",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Alvi", func(t *testing.T) {
+		result := HelloWorld("Alvi")
+		require.Equal(t, "Hello Alvi", result, "Result must be 'Hello Alvi'")
+	})
+	t.Run("Geovanny", func(t *testing.T) {
+		result := HelloWorld("Geovanny")
+		require.Equal(t, "Hello Geovanny", result, "Result must be 'Hello Geovanny'")
+	})
+}
+
+func TestMain(m *testing.M) {
+	// before
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run()
+
+	// after
+	fmt.Println("AFTER UNIT TEST")
+}
+
 func TestSkip(t *testing.T) {
 	if runtime.GOOS == "darwin" {
 		t.Skip("Can't run on Mac OS")
